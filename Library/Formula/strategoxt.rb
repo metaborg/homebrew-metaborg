11fa583d7bd255b5e5ede26d3e80a71ba0bf442d
Hendrik van Antwerpen <hendrik@van-antwerpen.net>
2015-01-09 16:22:01 +0100

    Added strategoxt formula.

---
+++ Library/Formula/strategoxt.rb
@@ -0,0 +1,36 @@
+require "formula"
+
+class Strategoxt < Formula
+  homepage "http://metaborg.org/stratego/"
+  version "20140618"
+  url "https://github.com/metaborg/strategoxt/releases/download/baselines/baseline-#{version}/strategoxt-distrib.tar"
+  sha1 "46764cbee62d932fdb040125348ee06000bbf028"
+
+  def install
+    share.install Dir["share/*"]
+    puts Dir["share/lib*/*.jar"]
+    commands = {
+      "gen-renamed-sdf-module" => "run org.strategoxt.tools.main-gen-renamed-sdf-module",
+      "gen-sdf-mix" => "run org.strategoxt.tools.main-gen-sdf-mix",
+      "pack-sdf" => "run org.strategoxt.tools.main-pack-sdf",
+      "parse-pp-table" => "-Xss8m run org.strategoxt.tools.main-parse-pp-table",
+      "parse-stratego" => "-Xss8m run org.strategoxt.tools.parse-stratego-io",
+      "ppgen" => "run org.strategoxt.tools.main-ppgen",
+      "rtg2sig" => "run org.strategoxt.tools.main-rtg2sig",
+      "sdf2parenthesize" => "run org.strategoxt.tools.main-sdf2parenthesize",
+      "sdf2rtg" => "run org.strategoxt.tools.main-sdf2rtg",
+      "strj" => "-Xss8m -Xmx1024m run"
+    }
+    commands.each do |command,arguments|
+      (bin+command).write <<-EOS.undent
+        #!/bin/bash
+        exec java -cp #{share}/strategoxt/strategoxt/strategoxt.jar #{arguments} $@
+      EOS
+    end
+  end
+
+  test do
+    system bin+"strategoxt"
+  end
+
+end
--- README.md
+++ README.md
@@ -1,2 +1,16 @@
-# homebrew
-Metaborg Homebrew Tap
+# MetaBorg Homebrew Tap
+
+To install MetaBorg formulas with Homebrew, add this tap with
+```brew tap https://github.com/hendrikvanantwerpen/homebrew.git```.
+
+Now install Stratego/XT with
+```brew install strategoxt```
+
+## Notes for Linux
+
+You can use this way on linux as well, if you install the Homebrew
+clone Linuxbrew (https://github.com/Homebrew/linuxbrew).
+
+If `brew install` complains that it has no compiler to use, try to
+pass one explicitly on the commandline, as in:
+```brew install strategoxt --cc=gcc-4.9`````
