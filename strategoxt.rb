f2d61566060e47ca8b6981e1ff5172fedd4790b9
Vlad Vergu <v.vergu@gmail.com>
2015-01-12 15:22:02 +0100

    Update strategoxt.rb

--- strategoxt.rb
+++ strategoxt.rb
@@ -3,7 +3,7 @@ require "formula"
 class Strategoxt < Formula
   homepage "http://metaborg.org/stratego/"
   version "20140618"
-  revision 2
+  revision 3
   url "https://github.com/metaborg/strategoxt/releases/download/baselines/baseline-#{version}/strategoxt-distrib.tar"
   sha1 "46764cbee62d932fdb040125348ee06000bbf028"
 
@@ -19,7 +19,7 @@ class Strategoxt < Formula
       "rtg2sig" => "run org.strategoxt.tools.main-rtg2sig",
       "sdf2parenthesize" => "run org.strategoxt.tools.main-sdf2parenthesize",
       "sdf2rtg" => "run org.strategoxt.tools.main-sdf2rtg",
-      "strj" => "-Xss8m -Xmx1024m"
+      "strj" => "-Xss8m -Xmx1024m org.strategoxt.strj.Main"
     }
     commands.each do |command,arguments|
       (bin+command).write <<-EOS.undent
