0353f863873f2c80d7b5265d52ba56b077c60304
Bruce Collie <brucecollie82@gmail.com>
2020-06-18 14:42:49 +0100

    Add spoofax-nightly cask

---
+++ Casks/spoofax-nightly.rb
@@ -0,0 +1,9 @@
+cask 'spoofax' do
+  version '2.6.0-SNAPSHOT'
+
+  url 'http://buildfarm.metaborg.org/job/metaborg/job/spoofax-releng/job/master/lastSuccessfulBuild/artifact/dist/spoofax/eclipse/spoofax-macosx-x64-jre.tar.gz'
+  name 'Spoofax'
+  homepage 'https://www.metaborg.org/'
+
+  app 'spoofax.app'
+end
