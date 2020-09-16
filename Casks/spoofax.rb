ce98bde89c316dc59913b53dbca35a7d1a3e3b6f
Alexander Ronald Altman <alexanderaltman@me.com>
2020-09-15 18:54:50 -0700

    Spoofax 2.5.11

--- Casks/spoofax.rb
+++ Casks/spoofax.rb
@@ -1,6 +1,6 @@
 cask 'spoofax' do
-  version '2.5.9'
-  sha256 '62dc7c3a9052acaa3bdb8524279859b9c9a75d7b68a9202cf242aba6b2ad5860'
+  version '2.5.11'
+  sha256 '22606482d1c24738ec79b0ae83b820ed81f3222a6c3bc18c6bb66015ca92401d'
 
   url "https://artifacts.metaborg.org/service/local/repositories/releases/content/org/metaborg/org.metaborg.spoofax.eclipse.dist/#{version}/org.metaborg.spoofax.eclipse.dist-#{version}-macosx-x64-jre.tar.gz"
   appcast 'https://github.com/metaborg/spoofax/releases.atom'
