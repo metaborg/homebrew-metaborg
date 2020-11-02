95a0a8d6d037a37dab3b0fdb5e4f39d1440d5b95
Alexander Ronald Altman <alexanderaltman@me.com>
2020-11-02 15:37:16 -0800

    Update spoofax from 2.5.11 to 2.5.12

--- Casks/spoofax.rb
+++ Casks/spoofax.rb
@@ -1,11 +1,11 @@
-cask 'spoofax' do
-  version '2.5.11'
-  sha256 '22606482d1c24738ec79b0ae83b820ed81f3222a6c3bc18c6bb66015ca92401d'
+cask "spoofax" do
+  version "2.5.12"
+  sha256 "f8899ffdfac8d4b948a834f711a7a0dd0feecfb0b3c03f845cb4c5099816a54c"
 
   url "https://artifacts.metaborg.org/service/local/repositories/releases/content/org/metaborg/org.metaborg.spoofax.eclipse.dist/#{version}/org.metaborg.spoofax.eclipse.dist-#{version}-macosx-x64-jre.tar.gz"
-  appcast 'https://github.com/metaborg/spoofax/releases.atom'
-  name 'Spoofax'
-  homepage 'https://www.metaborg.org/'
+  appcast "https://github.com/metaborg/spoofax/releases.atom"
+  name "Spoofax"
+  homepage "https://www.metaborg.org/"
 
-  app 'spoofax.app'
+  app "spoofax.app"
 end
